7c6f1d346cdb5cb896a2e131819db5aa82060074
Jeff Wear <jeff@mixmax.com>
2016-12-21 23:56:59 -0600

    Code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rollup-plugin-node-resolve-globals
-Test case for issue using rollup's `globals` option and `rollup-plugin-node-resolve` together
+
+Test case for issue using rollup's `globals` option and `rollup-plugin-node-resolve` together.
+
+To reproduce, uncomment the various cases in `rollup.config.js`
+and then run `npm run build`.
---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+/* jshint node:true, esnext:true */
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+  entry: 'index.js',
+  globals: {
+    $: '$'
+  },
+  plugins: [
+    // Cases (uncomment code to see):
+
+    // 1. no nodeResolve - works
+    // // nodeResolve()
+
+    // 2. nodeResolve - fails even though it should work?
+    // nodeResolve()
+
+    // 3. nodeResolve with skip - works but should be unnecessary
+    // nodeResolve({
+    //   skip: ['$']
+    // })
+  ],
+  targets: [{
+    format: 'es',
+    dest: 'bundle.js'
+  }]
+};
